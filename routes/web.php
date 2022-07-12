084da9027bfcf22688e675a30ff0278ac333c5d5
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-07-12 15:24:44 +0900

    編集処理

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
 Route::post('/tweet/create',  \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
+Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)
+->name('tweet.update.index')->where('tweetID', '[0-9]+');
+Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)
+->name('tweet.update.put')->where('tweetID', '[0-9]+');
